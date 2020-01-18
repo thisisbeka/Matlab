07d4f14e3cdd88189893bcff053355a9ff4af121
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2020-01-18 21:57:50 +0300

    Update 4.m

--- 4 zadanie/4.m
+++ 4 zadanie/4.m
@@ -1,1 +1,8 @@
-
+fplot(@(x) sin(2*x-3)-3,[-10 -3],'r')
+hold on
+fplot(@(x) (abs(x+3)^(1/3)-2),[-3 6],'b')
+hold on
+fplot(@(x) (x^2/2-3*x),[6 15],'g')
+hold off
+grid on
+legend({'F_1','F_2','F_3'},'Location','northwest')
